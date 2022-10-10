f1bc76cb225e8a44fff28396958efd9f9a1c7134
Michael <mickeymurage@gmail.com>
2022-10-10 22:44:48 +0300

    managed remote stock deduction through sales on the frontend

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react'
+import { toast, ToastContainer } from 'react-toastify'
 
 function Home() {
 	const [data, setData] = useState([])
 	const [cart, setCart] = useState([])
 	const [err, setErr] = useState('')
-	// const [num, setNum] = useState(1)
+	// const [tot, setTot] = useState(0)
+
+	let currentTotal = cart.reduce((p, c)=> (p + c.price), 0)
 
 	useEffect(()=>{
 		fetch('/api/items')
@@ -28,6 +31,46 @@ function Home() {
 		setCart([...cart].concat(item))
 	}
 
+	const generateSale = (e) =>{		
+		// ensure cart is not empty
+		try {
+			console.log(cart[0].user_id);
+		} catch (error) {
+			toast('cart is empty')
+			return
+		}
+
+		//generate a sales record
+		let items_sold = (cart.map(elem=> elem.name))
+		
+		fetch('/api/sales',{
+			method: "POST",
+			headers: {"Content-Type": "application/json"},
+			body: JSON.stringify({
+				user_id: cart[0].user_id,
+				customer_id: 5,
+				items_sold: `${items_sold}`,
+				total: currentTotal
+			})
+		})
+		.then((res)=>{
+			if(res.ok){
+				//reset the cart
+				setCart([])
+				// update the quantity
+				fetch('/api/quantity',{
+					method: "PATCH",
+					headers: {"Content-Type": "application/json"},
+					body: JSON.stringify({items: items_sold})
+				})
+			}else{
+				res.json().then(()=>toast('Something went wrong'))
+			}
+		})
+		.catch((err)=>console.log(err.message))
+		
+	}
+
 	return (
 		<div className='row '>
 				<div className='col col-sm-3 col-md-3 col-lg-3 sidebar-parent' style={{backgroundColor: "var(--navy)", color: "var(--blue)"}}>
@@ -38,7 +81,7 @@ function Home() {
 								<div className='row d-flex border cart' key={ind} id={ind}>
 									<p className='col '>{item.name.split('_').join(' ')}</p>
 									<p className='col'>1</p>
-									<p className='col'>80 </p>
+									<p className='col'>{item.price} </p>
 									<button className='btn remove' onClick={()=>removeFromCart(ind)}>Remove</button>
 								</div>
 							)
@@ -46,9 +89,9 @@ function Home() {
 					}
 					</div>
 				<div className='sidebar-print sticky-bottom'>
-					<p className='ml-2'>Total: {cart[1] ? cart.reduce((p, c)=> (p + c.quantity), 0) : 0}</p>
+					<p className='ml-2'>Total: {cart[1] ? currentTotal : 0}</p>
 					<div className='text-center mt-2'>
-						<button className='btn btn-primary'>Print</button>
+						<button className='btn btn-primary' onClick={generateSale}>Print</button>
 					</div>
 				</div>
 				
@@ -60,17 +103,18 @@ function Home() {
 							return (
 								<div className='card m-2 item' style={{backgroundColor: "var(--blue)", color: "var(--navy)"}} key={item.id} id={item.name} onClick={()=>addToCart(item.id)}>
 									<img src={require('../assets/Veggie.jpeg')} className='item-img' alt='item'/>
+									<h4>{item.quantity} remaining</h4>
 									<h3>{item.name.split('_').join(' ')}</h3>
 									<h4>{item.category.name}</h4>
-									<h4>Quantity: {item.quantity}</h4>
-									<h4>Price: $80</h4>
+									<h4 >Price: Kshs <span style={{color: "var(--light)"}}>{item.price}</span></h4>
 									<div >
-										<button style={{backgroundColor: "var(--dark)", color: "var(--blue)"}}>Add to cart</button>
+										<button className='btn btn-primary' style={{ color: "var(--navy)"}}>Add to cart</button>
 									</div>
 								</div>
 							)
 						})
 					}
+					<ToastContainer/>
 				</div>
 			</div>
 		</div>
