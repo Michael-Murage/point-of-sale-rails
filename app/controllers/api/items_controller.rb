fb66d3a2c72d186f58bfc27ed2ae73264760b799
Michael <mickeymurage@gmail.com>
2022-10-18 07:58:31 +0300

    enabled user to sell and reduce quantity

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -1,4 +1,5 @@
 class Api::ItemsController < ApplicationController
+	skip_before_action :authorized, only: [:index]
 	def index
 		item = Item.all
 		render json: item, each_serializer: ItemShowSerializer, status: :ok
