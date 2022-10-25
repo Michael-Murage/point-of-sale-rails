1a411e8d3c4cb9f6748758a012a3bf6027d05f13
Michael <mickeymurage@gmail.com>
2022-10-25 10:19:16 +0300

    re-enabled validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,6 +4,6 @@ class Category < ApplicationRecord
 	has_many :suppliers, through: :items
 	has_many :users, through: :items
 
-	# validates :name, presence: true
-	# validates :name, uniqueness: true
+	validates :name, presence: true
+	validates :name, uniqueness: true
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,13 +4,13 @@ class Item < ApplicationRecord
 	belongs_to :category
 	has_many :sales
 
-	# validates :user_id, numericality: {greater_than: 0}
-	# validates :supplier_id, numericality: {greater_than: 0}
-	# validates :category_id, numericality: {greater_than: 0}
-	# validates :name, presence: true
-	# # validates :quantity, numericality: {greater_than: 0}
-	# validates :price, numericality: {greater_than: 0}
-	# validates :cost, numericality: {greater_than: 0}
-	# validates :name, uniqueness: true
-	# validates :name, length: {maximum: 18}
+	validates :user_id, numericality: {greater_than: 0}
+	validates :supplier_id, numericality: {greater_than: 0}
+	validates :category_id, numericality: {greater_than: 0}
+	validates :name, presence: true
+	# validates :quantity, numericality: {greater_than: 0}
+	validates :price, numericality: {greater_than: 0}
+	validates :cost, numericality: {greater_than: 0}
+	validates :name, uniqueness: true
+	validates :name, length: {maximum: 18}
 end
