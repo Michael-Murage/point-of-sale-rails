a119e2de3b33df23506db631a959f66bb1bb0f6b
Michael <mickeymurage@gmail.com>
2023-08-02 22:19:00 +0300

    added common input reusable component

---
+++ client_v2/src/reusables/Input.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+
+type Props = {
+  label: string,
+  labelClassName?: string,
+  containerClassName?: string,
+  type?: 'text' | 'date' | 'number' | 'email' | 'password',
+  inputClassName?: string,
+  value: string | number,
+  handleChange: Function,
+  required?: boolean
+  id?: string
+}
+
+function Input(
+  { 
+    label, 
+    labelClassName, 
+    containerClassName, 
+    type = 'text',
+    inputClassName,
+    value,
+    handleChange,
+    required = false,
+    id
+  }: Props
+) 
+{
+  return (
+    <div className={`${containerClassName} flex flex-col`}>
+      <label 
+        htmlFor="" 
+        className={`${labelClassName} text-left text-gray-800 font-light`}
+      >
+        { label }
+        <span 
+          className={`${required ? '' : 'hidden'} text-red-500`}
+          title={`${label} is required`}
+        > *</span>
+      </label>
+      <input 
+        type={type} 
+        name="" 
+        id={id} 
+        className={`${inputClassName} border rounded-md`} 
+        value={value}
+        onChange={(e) => handleChange(e)}
+      />
+    </div>
+  )
+}
+
+export default Input
