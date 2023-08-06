b0994d55f745c5182485dc281cfbbb6885c4fe18
Michael <mickeymurage@gmail.com>
2023-08-06 19:23:00 +0300

    added login page and styles sheet

--- client_v2/src/components/Login.tsx
+++ client_v2/src/components/Login.tsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react'
 import { toast } from 'react-toastify'
+import Input from '../reusables/Input'
+import '../styles/Login.css'
+import Button from '../reusables/Button'
 
 function Login({ setCurrentUser, currentUser }): JSX.Element {
 	const [err, setErr] = useState(null)
@@ -38,12 +41,41 @@ function Login({ setCurrentUser, currentUser }): JSX.Element {
 	}
   
   return (
-    <div>
-      <div className="">
-        <div>
-          <label htmlFor="email">Email</label>
-          <input type="email" name="email" id="" />
-        </div>
+    <div className='border border-blue-300 cont bg-blue-100'>
+      <div className="border px-6 rounded-lg shadow-md w-50 mx-auto form-container text-center bg-white">
+        <form action="" className='py-6 px-3'>
+          <h2
+            className='font-semibold my-3 text-2xl'
+          >
+            SIGN IN
+          </h2>
+          <Input
+            label='EMAIL'
+            id='email'
+            value={cred.name}
+            labelClassName='text-xs'
+            containerClassName='my-3 py-2'
+            inputClassName=''
+            required
+            handleChange={handleChange}
+          />
+          <Input
+            type='password'
+            id='password'
+            label='PASSWORD'
+            labelClassName='text-xs'
+            containerClassName='my-3 py-2'
+            value={cred.name}
+            required
+            handleChange={handleChange}
+          />
+
+          <Button
+            type='submit'
+            buttonClassName='bg-blue-600 hover:bg-blue-400 rounded-md px-6 py-2 text-white hover:text-gray-600 my-6 text-sm'
+            value='LOGIN'
+          />
+        </form>
       </div>
     </div>
   )
